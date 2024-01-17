aeafddf73d8becf457d871c85028f861ab2af374
ersir <ersir@SIRWAN-PC>
2024-01-17 11:22:33 +0330

    Add project files.

---
+++ FontsBug/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FontsBug
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
